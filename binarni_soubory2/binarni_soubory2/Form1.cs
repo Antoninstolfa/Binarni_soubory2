9ebe3b36cd1eb12a8b814c3d54ad9ba629ed0f27
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2023-03-20 09:26:24 +0100

    Add files via upload

---
+++ binarni_soubory2/binarni_soubory2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace binarni_soubory2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader cistcisla = new StreamReader("cisla.txt");
+            FileStream datovytok = new FileStream("cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter zapisovac = new BinaryWriter(datovytok, Encoding.GetEncoding("windows-1250"));
+            while(!cistcisla.EndOfStream)
+            {
+                long cislo = long.Parse(cistcisla.ReadLine());
+                zapisovac.Write(cislo);
+            }
+            //zapisovac.Close();
+            BinaryReader ctenar = new BinaryReader(datovytok, Encoding.GetEncoding("windows-1250"));
+            ctenar.BaseStream.Position = 0;
+            while(ctenar.BaseStream.Position < ctenar.BaseStream.Length)
+            {
+                listBox1.Items.Add(ctenar.ReadInt64());
+                listBox1.Items.Add(sizeof(long));
+            }
+            datovytok.Close();
+            cistcisla.Close();
+            //ctenar.Close();
+        }
+    }
+}
